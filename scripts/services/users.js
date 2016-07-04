9b4a3fb49872a4d58c43fbe0078e0c04e793f025
Patrick Lai <patrick.lai@lifeletters.com.au>
2016-07-04 12:00:11 +1000

    Updated to consume API routes

--- scripts/services/users.js
+++ scripts/services/users.js
@@ -8,228 +8,219 @@
  * Service in the lifelettersApp.
  */
 angular.module('life.users')
-  .provider('users', function () {
-    
-    var rootUrl = null,
-        loginPath = '/login',
-        listDoctorsPath = '/doctors',
-        appointmentsPath = '/appointments',
-        behaviours = {},
-        doctorCookieName = 'lifeLetterCurrentDoctor'+(window.cookies ? '-'+window.cookies:''),
-        patientCookieName = 'lifeLetterCurrentPatient'+(window.cookies ? '-'+window.cookies:''),
-        userTypes = [
-          'Clinician',
-          'Patient',
-        ];;
+    .provider('users', function() {
 
-    this.setUrl = function(url) { 
-      rootUrl = url; 
-    };
-    this.setLoginPath = function(url) { 
-      logingUrl = url; 
-    };
-    this.addBehaviour = function(name, func) { 
-      behaviours[name] = func; 
-    };
+        var rootUrl = null,
+            loginPath = '/login',
+            listDoctorsPath = '/doctors',
+            appointmentsPath = '/appointments',
+            behaviours = {},
+            doctorCookieName = 'lifeLetterCurrentDoctor' + (window.cookies ? '-' + window.cookies : ''),
+            appointmentCookieName = 'lifeLetterCurrentAppointment' + (window.cookies ? '-' + window.cookies : ''),
+            tokenCookieName = 'lifeLetterToken' + (window.cookies ? '-' + window.cookies : ''),
+            userTypes = [
+                'Clinician',
+                'Patient',
+            ];;
 
-    this.$get = function ($rootScope, $http, $location, $cookies, $timeout, $q, $log, lodash) {
-      if ( !rootUrl ) {
-        $log.warn('please set the user service URL via the usersProvider');
-        return;
-      }
-
-      // Expose the user to the view
-      $rootScope.currentDoctor = null;
-      $rootScope.currentPatient = null;
-
-      // Adds instance specific methods to the user object
-      function initUser(user) {
-        // Avoid repeating
-        if ( user.sync ) { 
-          return user; 
-        }
-
-        user.name = function() { 
-          return lodash.concat(user.title?[user.title]:[], [user.firstName, user.lastName]).join(' ');
+        this.setUrl = function(url) {
+            rootUrl = url;
+        };
+        this.setLoginPath = function(url) {
+            logingUrl = url;
+        };
+        this.addBehaviour = function(name, func) {
+            behaviours[name] = func;
         };
 
-        // Add helper methods for type, e.g. isPatient
-        user.is = function(type) {
-          return user.userType === type; // simple match for speed reasons
-        }; 
-        angular.forEach(userTypes, function(type) {
-          user['is'+type] = function() { return user.is(type); }
-        });
+        this.$get = function($rootScope, $http, $location, $cookies, $timeout, $q, $log, lodash) {
+            if (!rootUrl) {
+                $log.warn('please set the user service URL via the usersProvider');
+                return;
+            }
 
-        user.fetch = function(type, id) {
-          var path = rootUrl+'users/'+user.id+'/'+type+(id ? '/'+id : '');
+            // Expose the user to the view
+            $rootScope.currentDoctor = null;
+            $rootScope.currentAppointment = null;
 
-          return $http.get(path)
-            .then(function(response) {
-              return response.data;
-            }, function(e) {
-              $log.warn(e);
-              return $q.reject(e);
-            });
-        }
+            function setCurrentDoctor(doctor) {
+                // Ensure the doctor has the madatory details
+                var minimum = [
+                    'id'
+                ];
 
-        user.create = function(type, body) {
-          var path = rootUrl+'users/'+user.id+'/'+type;
-          return $http.post(path, body)
-            .then(function(response) {
-              return response.data;
-            }, function(e) {
-              $log.warn(e);
-              return $q.reject(e);
-            });
-        }
-
-        user.isLoggedInUser = function() { 
-          return user.id === $rootScope.currentDoctor.clinicianCode; 
-        }
-
-        user.sync = function() {
-          var path = rootUrl+'users/'+user.id;
-
-          return $http.put(path, user)
-            .then(function(response) {
-              return response.data;
-            }, function(e) {
-              $log.error(e);
-              return $q.reject();
-            });
-        };
-
-        // Add custom behaviour to the user
-        angular.forEach(behaviours, function(func, name) {
-          user[name] = function() { return func(user, arguments); };
-        });
-
-        return user;
-      }
-
-      function setCurrentDoctor(doctor) {
-        // Ensure the doctor has the madatory details
-        var minimum = [
-              'clinicianCode'
-            ];
-
-        if ( lodash.intersection(minimum, lodash.keys(doctor)).length !== minimum.length ) {
-          $log.warn('missing details', minimum, lodash.keys(doctor));
-          return false;
-        }
-
-        // initUser(doctor);
-        // $http.defaults.headers.common.Authorization = doctor.authToken;
-        $cookies.putObject(doctorCookieName, doctor, {
-          expires: moment().add(2,"days").toDate()
-        });
-
-        return $rootScope.currentDoctor = doctor;
-      }
-
-      function setCurrentPatient(patient) {
-        // Ensure the patient has the madatory details
-        var minimum = [
-              'userid'
-            ];
-
-        if ( lodash.intersection(minimum, lodash.keys(patient)).length !== minimum.length ) {
-          $log.warn('missing details', minimum, lodash.keys(patient));
-          return false;
-        }
-
-        // initUser(patient);
-        // $http.defaults.headers.common.Authorization = user.authToken;
-        $cookies.putObject(patientCookieName, patient);
-        return $rootScope.currentPatient = patient;
-      }
-
-      function clearCurrentUser() {
-        $http.defaults.headers.common.Authorization = '';
-        $rootScope.currentPatient = null
-        $rootScope.currentDoctor = null;
-        $cookies.remove(doctorCookieName);
-        $cookies.remove(patientCookieName);
-      }
-
-      // Restore the user from previous session
-      if ( _.isObject( $cookies.getObject(doctorCookieName) ) ) {
-        setCurrentDoctor( $cookies.getObject(doctorCookieName) );
-      }
-
-      // Restore patient too
-      if ( _.isObject( $cookies.getObject(patientCookieName) ) ) {
-        setCurrentPatient( $cookies.getObject(patientCookieName) );
-      }
-      
-      return {
-        loginPath: loginPath,
-        userTypes: userTypes,
-
-        // TODO: make it actually login
-        setCurrentDoctor: function(doctor) {
-          setCurrentDoctor(doctor);
-        },
-
-        setCurrentPatient: function(patient){
-          setCurrentPatient(patient)
-        },
-
-        logOut: function() {
-          clearCurrentUser();
-        },
-
-        listDoctors: function(clinicID){
-          var path = rootUrl+clinicID+listDoctorsPath
-          return $http.get(path)
-            .then(function(response) {
-              return response.data;
-            }, function(e) {
-              $log.warn(e);
-              return $q.reject(e);
-            });
-        },
-
-        listPatientForCurrentUser: function(){
-          // Get appontment list
-          var path = 'https://private-e81a7-lifelettersclinicapp.apiary-mock.com/appointments'
-
-          // var path = rootUrl+appointmentsPath
-
-          return $http.get(path)
-            .then(function(response) {
-
-              // Filter by doctor
-              var patientList = lodash.filter(response.data, function(appointment){
-                return true
-                // return appointment.doctor.clinicianCode == $rootScope.currentDoctor.clinicianCode
-              })
-
-              // Filter down to patient objects only
-              patientList = lodash.map(patientList, function(appointment){
-                // Build a mock patient object for now
-                var patient = {
-                  "name": appointment.firstName+" "+appointment.lastName,
-                  "userid": appointment.id,
-                  "photo": appointment.pic,
-                  "dob": appointment.dob,
-                  "time": appointment.time,
-                  "interests": ["eating","blogging","selfies"],
-                  "reason": "Suffering from lower-back pain after playing soccer on the weekend"
+                if (lodash.intersection(minimum, lodash.keys(doctor)).length !== minimum.length) {
+                    $log.warn('missing details', minimum, lodash.keys(doctor));
+                    return false;
                 }
 
-                return patient
-                // return appointment.patient
-              })
+                // initUser(doctor);
+                // $http.defaults.headers.common.Authorization = doctor.authToken;
+                $cookies.putObject(doctorCookieName, doctor, {
+                    expires: moment().add(2, "days").toDate()
+                });
 
-              return patientList;
-            }, function(e) {
-              $log.warn(e);
-              return $q.reject(e);
-            });
-        }
+                return $rootScope.currentDoctor = doctor;
+            }
 
-      };
-    };
-  });
+            function setToken(token) {
+                $http.defaults.headers.common.Authorization = token;
+            }
+
+            function setCurrentAppointment(appointment) {
+                // Ensure the patient has the madatory details
+                var minimum = [
+                    'id'
+                ];
+
+                if (lodash.intersection(minimum, lodash.keys(appointment)).length !== minimum.length) {
+                    $log.warn('missing details', minimum, lodash.keys(appointment));
+                    return false;
+                }
+
+                $cookies.putObject(appointmentCookieName, appointment);
+                return $rootScope.currentAppointment = appointment;
+            }
+
+            function clearCurrentUser() {
+                $http.defaults.headers.common.Authorization = '';
+                $rootScope.currentAppointment = null
+                $rootScope.currentDoctor = null;
+                $cookies.remove(doctorCookieName);
+                $cookies.remove(appointmentCookieName);
+                $cookies.remove(tokenCookieName)
+            }
+
+            // Get a doctor by ID
+            function fetchDoctorById(id) {
+                var path = "https://auth.lifeletters.xyz/doctors/" + id;
+
+                return $http.get(path)
+                    .then(function(response) {
+                        return response.data;
+                    }, function(e) {
+                        $log.warn(e);
+                        return $q.reject(e);
+                    });
+            }
+
+
+            function fetchPatientById(id) {
+                var path = "https://auth.lifeletters.xyz/patients/" + id;
+
+                return $http.get(path)
+                    .then(function(response) {
+                        return response.data;
+                    }, function(e) {
+                        $log.warn(e);
+                        return $q.reject(e);
+                    });
+            }
+
+            function fetchAppointments(){
+                    // Get appontment list
+                    var path = rootUrl + appointmentsPath;
+
+                    return $http.get(path)
+                        .then(function(response) {
+
+                            if (!response.data.success) {
+                                return [];
+                            }
+
+                            var appointmentList = response.data.consults;
+
+                            // Append full Doctor and Patient from this
+
+                            var promises = [];
+
+                            angular.forEach(appointmentList, function(appointment) {
+
+                                // Gotta defer the promises so we get the whole object
+                                var deffered  = $q.defer();
+
+                                fetchDoctorById(appointment.doctorId).then(function(doctor){
+                                  deffered.resolve(doctor);
+                                  appointment.doctor = doctor[0];
+                                  appointment.doctor.photo = "http://www.coastalwatch.com/Content/Images/profile_noImage_sm.png";
+                                });
+                                
+                                fetchPatientById(appointment.patientId).then(function(patient){
+                                  deffered.resolve(patient);
+                                  appointment.patient = patient[0]
+                                  // Lets add a picture
+                                  appointment.patient.photo = "http://www.coastalwatch.com/Content/Images/profile_noImage_sm.png";
+                                })
+
+                                promises.push(deffered.promise);
+
+                            });
+
+                            return $q.all(promises).then(function(){
+                              return appointmentList
+                            })
+
+                        }, function(e) {
+                            $log.warn(e);
+                            return $q.reject(e);
+                        });
+            }
+
+            // Restore the user from previous session
+            if (_.isObject($cookies.getObject(doctorCookieName))) {
+                setCurrentDoctor($cookies.getObject(doctorCookieName));
+            }
+
+            // Restore appointment too
+            if (_.isObject($cookies.getObject(appointmentCookieName))) {
+                setCurrentAppointment($cookies.getObject(appointmentCookieName));
+            }
+
+            // Restore Authorization cookie
+            if (_.isString($cookies.getObject(tokenCookieName))) {
+                setToken($cookies.getObject(tokenCookieName));
+            }
+
+            return {
+                loginPath: loginPath,
+                userTypes: userTypes,
+
+                // TODO: make it actually login
+                setCurrentDoctor: function(doctor) {
+                    setCurrentDoctor(doctor);
+                },
+
+                setCurrentAppointment: function(apt) {
+                    setCurrentAppointment(apt)
+                },
+
+                isLoggedIn: function() {
+                    return $http.defaults.headers.common.Authorization;
+                },
+
+                logIn: function(username, password) {
+                    // Try to log in
+                    var path = "https://auth.lifeletters.xyz/login"
+
+                    return $http.post(path, { username: username, password: password }).then(function(response) {
+                        //success
+                        $http.defaults.headers.common.Authorization = response.data.access_token;
+                        $cookies.putObject(tokenCookieName, response.data.access_token);
+                        return response.data.access_token
+                    }, function(error) {
+                        //error, display validation error
+                        console.log("login error");
+                    });
+
+                },
+
+                logOut: function() {
+                    clearCurrentUser();
+                },
+
+                fetchAppointments: function() {
+                  return fetchAppointments();
+                }
+            };
+        };
+    });
